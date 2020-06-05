81f8406873b8faffc10094d3cf5067254b60d764
suxinying <34708049+suxinying@users.noreply.github.com>
2020-06-05 09:16:46 +0800

    Update index.js

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { WebView, View, StyleSheet, Platform } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
+import {WebView} from 'react-native-webview';
 
 export default class App extends Component {
 
